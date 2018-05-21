3c1a64e1458593512078a5b0ffc75f6430fc9d7b
matt-s-gibbs <gibbs.ms@gmail.com>
2018-05-21 19:40:49 +0930

    testing evaluate metrics

--- helpers.R
+++ helpers.R
@@ -1,9 +1,19 @@
+library(SWTools)
 library(zoo)
 warmup<-30 #change to input variable
 minQ<-2500
+minDO<-2/1000
 
 A<-VeneerGetTSbyVariable("Flow")
 A<-window(A,start=start(A)+warmup)
 FlowT<-apply(A,2,function(x) sum(x<minQ)/length(x))
 
 A<-VeneerGetTSbyVariable("Constituents@DO@Downstream Flow Concentration")
+#units are Units":"kg\/m³, need to fix in package
+DOT<-apply(A,2,function(x) sum(x<minDO)/length(x))
+
+#derive velocity from volume
+
+#cbind(DOT,FlowT)
+#melt
+#ggplot shading by %age.
